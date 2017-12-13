5684712de6258360d17fbec5bbc548bf808cf40c
betegon <miguelbetegongarcia@gmail.com>
2017-12-13 15:56:27 +0100

    add string_to_bits and bits_to_string functions.

--- des.py
+++ des.py
@@ -1,19 +1,17 @@
 """
-key => key we will use to chyper and decypher
-
-plaintext => text to cypher/decypher
+key: key we will use to chyper and decypher
+text_plain: text to cypher/decypher
 """
-
 import matrices as m
 
 # CONSTANTS
-BLOCK_SIZE = 8  # Block size of DES = 8Bytes=64bits
+BLOCK_SIZE = 8  # Block size of DES = 8Bytes=64bits, sames as Key size.
 PADDING = "0"   # we will fill the plaintext with 0s  if needed
 
 
 # check key length
 def check_key_length(key):
-    if len(key) != 8:
+    if len(key) != BLOCK_SIZE:
         raise ValueError("DES Key size not valid, it should be 8 Bytes long.\n")
     else:
         print("Input Key is valid.")
@@ -25,6 +23,27 @@ def check_input(data):
         raise ValueError("Input cannot be empty.")
     else:
         print("Input data is valid.\n")
+        
+# String (ASCII) to bits        
+def string_to_bits(s):
+    result = []
+    for c in s:
+        bits = bin(ord(c))[2:] 
+        # ord(c) returns the value of the byte when the argument is an 8-bit string
+        # bin(ord(c)) as c is a string, returns  0b1100001(example) where 0b is like 0x for hexs, 0b indicates it is bin.
+        # bin(ord(c))[2:] we dont want the two first elements (0b)
+        bits = '00000000'[len(bits):] + bits #fill with 0s in the left until it reachs 8. 
+        # len(bits) normally is < 8 because bin() dos not show the left 0s
+        result.extend([int(b) for b in bits]) # put bits (a string) in an array.
+    return result
+
+# bits to string (ASCII)
+def bits_to_string(bits):
+    chars = []
+    for b in range(len(bits) / 8):
+        byte = bits[b*8:(b+1)*8] # byte = 8 bits, 0-7, 8-15...
+        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2))) # int((''.join...),2)=>base 2 (binary, 0 or 1)
+    return ''.join(chars) #from array (chars) to string
 
 
 # add padding to text
@@ -32,35 +51,52 @@ def add_padding(text):
     if len(text) % BLOCK_SIZE != 0:
         while len(text) % BLOCK_SIZE != 0:
             text += PADDING
-
     return text
 
 
-# remove paading to paddedtext 
+# remove padding to paddedtext 
 def remove_padding(text):
     text = text[:-BLOCK_SIZE] + text[-BLOCK_SIZE:].rstrip(PADDING)
     return text
+    
+    
+# **********************
+# Key related operations
+# **********************
+
+#apply Matrix Permuted Choice 1 (pc1)
+def permuted_choice1 (key):
+    c0 = [] #initializing c0 array
+    d0 = [] #initializing d0 array
+    
+    return c0,d0
+
+
+
+
+#LAST FUNCTION RELATED TO KEY OPERATIONS
+#def key_expansion(key):
+#    kn = [[0] * 48] * 16
+#    return kn
+
+
 
 
 key = "01234567"  # NOT hex, normal string.
-plaintext = "holaholaa"
+text_plain = "h"
+
 check_key_length(key)
-check_input(plaintext)
-textpadded = add_padding(plaintext)
-print("original text: " + plaintext)
-print("text padded:   " + textpadded)
-textunppaded = remove_padding(textpadded)
-print("text unppaded: " + textunppaded)
+check_input(text_plain)
+text_padded = add_padding(text_plain)
+bits = string_to_bits(text_padded)
+string  = bits_to_string(bits)
+text_unppaded = remove_padding(text_padded)
+print text_plain
+print text_padded
+print bits
+print string
+print text_unppaded
 
-# IDEA TO CHANGE FROM ASCII(strings in python2) TO binary
-#  key_bin = ''.join(format(ord(x),'b') for x in key)  # change from string to bits
-#  print(key_bin)
-# print(key.encode("hex"))
-#  print("holi".encode("hex"))
-
-
-
-# TODO-me inverse naming: text_plain,text_padded, text_unpadder
 # TODO-me implement PKCS-5 in order to avoid problem with remove_padding (if text=12340, padded=>12340000, unpadded=>1234 with my actual function remove_padding). if PKCS-5 complicated to implement, just make a quick check in remove_padding: if mes
 # TODO-me optimize add_padding and remove_padding
 # TODO-me offer the option of getting key, data an so by terminal (maybe comment one of the options, by prompt or by code))
